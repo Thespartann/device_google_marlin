8d17c785b5a56b8fa42854fb827560ebb55c38f5
Thespartann <danilastefan69@yahoo.ro>
2020-02-01 09:15:56 +0000

    marlin/sailfish: Don't inherit vendor/google_devices
    * Not used anymore
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- lineage_sailfish.mk
+++ lineage_sailfish.mk
@@ -21,7 +21,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/google/marlin/device-sailfish.mk)
-$(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-sailfish.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
